67161f6b91756876178e6a2fd76a352eddf75148
MontacerElFazazi <montacer.elfazazielfazazi@cieep.com>
2024-01-18 11:40:17 +0100

    good

--- ejemplo4FirebaseDiTema2/ViewController.swift
+++ ejemplo4FirebaseDiTema2/ViewController.swift
@@ -7,11 +7,17 @@
 
 import UIKit
 import FirebaseAuth
+import FirebaseFirestore
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var txtTelefono: UITextField!
+    @IBOutlet weak var txtDireccion: UITextField!
     @IBOutlet weak var lbUserId: UILabel!
     @IBOutlet weak var lbEmail: UILabel!
+    
+    var db: Firestore!
+    var id: String!
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -22,6 +28,8 @@ class ViewController: UIViewController {
         if let user = Auth.auth().currentUser{
             lbEmail.text = user.email
             lbUserId.text = user.uid
+            db = Firestore.firestore()
+            id = user.uid
         }
     }
 
@@ -35,5 +43,42 @@ class ViewController: UIViewController {
             
         }
     }
+    
+    
+    @IBAction func btnGuardar(_ sender: Any) {
+        db.collection("users").document(id).setData(
+            [
+                "direccion":txtDireccion.text!,
+                "telefono": txtTelefono.text!
+            ]
+        )
+    }
+    
+    
+    @IBAction func btnRecuperar(_ sender: Any) {
+        db.collection("users").document(id).getDocument{
+            (documentSnapshot, error) in
+            
+            if let document = documentSnapshot, error == nil{
+                if let direccion = document.get("direccion") as? String{
+                    self.txtDireccion.text = direccion
+                }
+                if let telefono = document.get("telefono") as? String{
+                    self.txtTelefono.text = telefono
+                }
+            }else{
+                let alert = UIAlertController(title: "ERROR", message: "error inesperado: \(error.debugDescription)", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Aceptar", style: .default))
+                self.present(alert, animated: true, completion: nil)
+            }
+        }
+    }
+    
+    
+    @IBAction func btnEliminar(_ sender: Any) {
+        db.collection("users").document(id).delete();
+    }
+    
+    
 }
 
